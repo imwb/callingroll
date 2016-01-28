c5e54860baa25d45a94cbed840bf53e6a85f9e0f
wb <79497777@qq.com>
2016-01-28 17:58:53 +0800

    register layout finished

--- app/src/main/java/com/example/wb/calling/activity/LoginActivity.java
+++ app/src/main/java/com/example/wb/calling/activity/LoginActivity.java
@@ -1,17 +1,37 @@
 package com.example.wb.calling.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.example.wb.calling.R;
+import com.gc.materialdesign.views.ButtonFlat;
+
+import static com.example.wb.calling.R.id.btn_register;
 
 /**
  * Created by wb on 16/1/23.
  */
 public class LoginActivity extends Activity {
+    private ButtonFlat regBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         setContentView(R.layout.activity_login);
+        initView();
+    }
+
+    private void initView() {
+        regBtn = (ButtonFlat) findViewById(btn_register);
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/wb/calling/activity/RegisterActivity.java
@@ -0,0 +1,129 @@
+package com.example.wb.calling.activity;
+
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.wb.calling.R;
+import com.example.wb.calling.utils.RegexUtil;
+import com.gc.materialdesign.views.ButtonRectangle;
+import com.rengwuxian.materialedittext.MaterialEditText;
+import com.rengwuxian.materialedittext.validation.METValidator;
+
+import java.util.regex.Pattern;
+
+import static com.example.wb.calling.R.id.btn_register;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        initview();
+    }
+
+    private void initview() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.StatusBarColor));
+        }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        final MaterialEditText usernameEdt = (MaterialEditText) findViewById(R.id.edt_username);
+        usernameEdt.addValidator(new METValidator("请输入正确的用户名") {
+            @Override
+            public boolean isValid(@NonNull CharSequence text, boolean isEmpty) {
+                if(RegexUtil.isIDCode(text.toString())){
+                    username = text.toString();
+                    Log.d("name",username);
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+        });
+
+        final MaterialEditText pwEdt = (MaterialEditText) findViewById(R.id.edt_password);
+        pwEdt.addValidator(new METValidator("密码要大于六位") {
+            @Override
+            public boolean isValid(@NonNull CharSequence text, boolean isEmpty) {
+                if((text != null)&& (text.length()>=6)){
+                    password = text.toString();
+                    Log.d("pw",password);
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+        });
+
+
+        final MaterialEditText nameEdt = (MaterialEditText) findViewById(R.id.edt_name);
+        nameEdt.addValidator(new METValidator("要真实姓名哦") {
+            @Override
+            public boolean isValid(@NonNull CharSequence text, boolean isEmpty) {
+                Pattern pattern = Pattern.compile("^[\u4e00-\u9fa5]+$");
+                if(pattern.matcher(text).matches()){
+                    name = text.toString();
+                    Log.d("name",name);
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+        });
+
+        final MaterialEditText emailEdt = (MaterialEditText) findViewById(R.id.edt_email);
+        emailEdt.addValidator(new METValidator("邮箱地址不正确") {
+            @Override
+            public boolean isValid(@NonNull CharSequence text, boolean isEmpty) {
+
+                if(RegexUtil.isEmail(text.toString())){
+                    email = text.toString();
+                    Log.d("email",email);
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+        });
+
+        ButtonRectangle registerBtn = (ButtonRectangle) findViewById(btn_register);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(usernameEdt.validate() && pwEdt.validate() && nameEdt.validate()
+                        && emailEdt.validate()){
+                    Toast.makeText(RegisterActivity.this,username+password+name+email,Toast.LENGTH_SHORT)
+                            .show();
+                }else {
+                    Toast.makeText(RegisterActivity.this,"请核对注册信息",Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+        });
+
+    }
+
+}
